a12462b0958100c7d7519d95871167e80fd89d26
Pivotal <Suresh@Sureshs-Mac-mini.local>
2016-01-14 15:21:19 +0530

    Actions are done

---
+++ js/actions/FluxCartActions.js
@@ -0,0 +1,43 @@
+//
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var FluxCartConstants = require('../constants/FluxCartConstants');
+var FluxCartActions = {
+  receiveProduct:function(data){
+    var action = {
+      actionType:FluxCartConstants.RECEIVE_DATA,
+      data:data
+    };
+    AppDispatcher.handleAction(action);
+  },
+  selectProduct:function(index){
+    var action = {
+      actionType:FluxCartConstants.SELECT_PRODUCT,
+      data:index
+    };
+    AppDispatcher.handleAction(action);
+  },
+  addToCart:function(sku,update){
+    var action = {
+      actionType:FluxCartConstants.CART_ADD,
+      sku:sku,
+      update:update
+    };
+    AppDispatcher.handleAction(action);
+  },
+  removeFromCart:function(sku){
+    var action = {
+      actionType:FluxCartConstants.CART_REMOVE,
+      sku:sku
+    };
+    AppDispatcher.handleAction(action);
+  },
+  updateCartVisible:function(cartVisible){
+    var action = {
+      actionType:FluxCartConstants.CART_VISIBLE,
+      cartVisible: cartVisible
+    };
+    AppDispatcher.handleAction(action);
+  }
+};
+//
+module.exports = FluxCartActions;
