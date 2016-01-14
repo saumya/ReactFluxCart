8bea4c3664af93f139b730ef6d916481e637a796
Pivotal <Suresh@Sureshs-Mac-mini.local>
2016-01-14 13:39:51 +0530

    html and app view is added

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//
+var React = require ('react');
+var ReactDOM = require ('react-dom');
+var ProductData = require ('./ProductData');
+var CartAPI = require ('./utils/CartAPI');
+var FluxCartApp = require ('./components/FluxCartApp.react');
+// store and get Data
+ProductData.init();
+CartAPI.getProductData();
+// Render
+ReactDOM.render(<FluxCartApp>,document.getElementById('flux-cart'));
